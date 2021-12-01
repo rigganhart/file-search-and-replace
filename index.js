bc864e6ec2cb41a86e9f2118ab2ff1952d4fe599
Riggan Hart <rigganh@gmail.com>
2021-11-30 22:15:44 -0500

    remove notes

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const path = require('path')
 const fs = require('fs')
 
-/**
- * Requirements:
- * 1. Accept a file as Input
- * 2. accept a string as "search Paramaeter"
- * 3. Accept a string as "replace parameter"
- * 4. Count all occurances of "search Parameter"
- * 5. replace all occurances of "search parameter" with "replacement parameter"
- * 6. save the replaced file
- * 7. output the total number of occurances of search parameter and the total number of replacements
- */
-
 const matcher = (searchParam, inputString, replaceString) => {
   const regex = new RegExp(searchParam, 'g')
   const matchedArr = inputString.match(regex)
